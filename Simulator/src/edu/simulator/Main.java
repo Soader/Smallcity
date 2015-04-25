89a8e6c118df92557f65192659924009b404fb09
Pawel Jasik <paweljasik93@gmail.com>
2015-04-25 19:24:05 +0200

    Deletion of unused parts of code.

--- Simulator/src/edu/simulator/Main.java
+++ Simulator/src/edu/simulator/Main.java
@@ -29,16 +29,15 @@ import org.lwjgl.opengl.DisplayMode;
 import org.newdawn.slick.opengl.Texture;
 import org.newdawn.slick.opengl.TextureLoader;
 
-
-
 public class Main {
-	Simulator simulator;
+	private Simulator simulator;
+	
 	public Main(Simulator simulator){
 		this.simulator = simulator;
 	}
 	
 	public static void main(String[] args) {
-	//	System.setProperty("org.lwjgl.opengl.Window.undecorated", "true");
+	
 		initDisplay();
 		initGL();
 		NString.initNString();
@@ -49,20 +48,17 @@ public class Main {
 	}
 	
 	private static void initDisplay(){
+		
 		try{
-		//	Display.setDisplayMode(new DisplayMode(600	, 768));
-			
-			setDisplayMode(1100, 650, false);
+			setDisplayMode(1100, 650);
 			Display.setTitle("Simulator");
 			Display.create();
-			
 			Display.setVSyncEnabled(true);
 		}
 		catch (LWJGLException ex){
 			Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
 		}						
 	}
-	
 	
 	private static void initGL(){
 		glMatrixMode(GL_PROJECTION);
@@ -88,76 +84,26 @@ public class Main {
 		}
 	}
 	
-	public static Texture loadTexture(String name){
-		
-		try {
-			return TextureLoader.getTexture("png", new FileInputStream(new File("res/" + name + ".png")));
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return null;
-	}
-	
-	
-	public static void setDisplayMode(int width, int height, boolean fullscreen) {
+	public static void setDisplayMode(int width, int height) {
 
-	 
 	    if ((Display.getDisplayMode().getWidth() == width) && 
-	        (Display.getDisplayMode().getHeight() == height) && 
-		(Display.isFullscreen() == fullscreen)) {
+	        (Display.getDisplayMode().getHeight() == height)) {
 		    return;
 	    }
 
 	    try {
-	        DisplayMode targetDisplayMode = null;
-			
-		if (fullscreen) {
-		    DisplayMode[] modes = Display.getAvailableDisplayModes();
-		    int freq = 0;
-					
-		    for (int i=0;i<modes.length;i++) {
-		        DisplayMode current = modes[i];
-						
-			if ((current.getWidth() == width) && (current.getHeight() == height)) {
-			    if ((targetDisplayMode == null) || (current.getFrequency() >= freq)) {
-			        if ((targetDisplayMode == null) || (current.getBitsPerPixel() > targetDisplayMode.getBitsPerPixel())) {
-				    targetDisplayMode = current;
-				    freq = targetDisplayMode.getFrequency();
-	                        }
-	                    }
-
-		
-			    if ((current.getBitsPerPixel() == Display.getDesktopDisplayMode().getBitsPerPixel()) &&
-	                        (current.getFrequency() == Display.getDesktopDisplayMode().getFrequency())) {
-	                            targetDisplayMode = current;
-	                            break;
-	                    }
-	                }
-	            }
-	        } else {
-	            targetDisplayMode = new DisplayMode(width,height);
-	        }
-
-	        if (targetDisplayMode == null) {
-	            System.out.println("Failed to find value mode: "+width+"x"+height+" fs="+fullscreen);
-	            return;
-	        }
-
+	        DisplayMode targetDisplayMode = new DisplayMode(width,height); 
 	        Display.setDisplayMode(targetDisplayMode);
-	        Display.setFullscreen(fullscreen);
-				
+	        
 	    } catch (LWJGLException e) {
-	        System.out.println("Unable to setup mode "+width+"x"+height+" fullscreen="+fullscreen + e);
+	        System.out.println("Unable to setup mode "+width+"x"+height+ e);
 	    }
 	}
-	
 	
 	private static void cleanUp(){
 		Display.destroy();
 	}
 
 }
+
+
